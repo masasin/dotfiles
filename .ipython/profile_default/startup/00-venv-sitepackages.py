b51489cf57bf4c245aee08cc862c1802336bb16b
Jean Nassar <jn.masasin+git@gmail.com>
2017-07-22 15:40:52 -0600

    Automatically run jupyter in the current venv
    If a virtual environment is activated, jupyter uses it in the path. At
    the same time, globally installed extensions are not affected.

---
+++ .ipython/profile_default/startup/00-venv-sitepackages.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+import sys
+
+
+virtualenv = os.environ.get('VIRTUAL_ENV')
+
+if virtualenv:
+    venv_path = list((Path(virtualenv)/'lib64').iterdir())[0]/'site-packages'
+    sys.path.append(str(venv_path))
